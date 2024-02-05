f04129c9a2384c40dad1568a50f5e1a08393ba89
adityarajiiit <148451902+adityarajiiit@users.noreply.github.com>
2024-02-05 22:42:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,15 @@ import { initializeApp } from 'firebase/app';
 
 
 import {resolveInclude} from "ejs";
-import firebaseConfig from "./public/firebase.js";
+const firebaseConfig = {
+    apiKey: "AIzaSyCmDQO5i940Lnh0KtRbjkuoRj1GFAZ80oI",
+    authDomain: "chat-c17d6.firebaseapp.com",
+    projectId: "chat-c17d6",
+    storageBucket: "chat-c17d6.appspot.com",
+    messagingSenderId: "590425564864",
+    appId: "1:590425564864:web:ca2104b170fa49a4c7af25",
+    measurementId: "G-T5KGJ89DJY"
+};
 
 const app = initializeApp(firebaseConfig);
 
