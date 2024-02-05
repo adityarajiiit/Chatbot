a40d9dc74fc5e945d2b453eae3281d56e5ba2c38
adityarajiiit <148451902+adityarajiiit@users.noreply.github.com>
2024-02-05 23:17:09 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -54,3 +54,28 @@ window.close();
         // ...
     });});
 
+document.getElementById("goo").addEventListener("touchstart",function() {
+    signInWithPopup(auth, provider)
+        .then((result) => {
+            // This gives you a Google Access Token. You can use it to access the Google API.
+            const credential = GoogleAuthProvider.credentialFromResult(result);
+            const token = credential.accessToken;
+            // The signed-in user info.
+            const user = result.user;
+            console.log(user);
+window.open("/chhat");
+window.close("/");
+window.close();
+
+            // ...
+        }).catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.customData.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        // ...
+    });});
+
